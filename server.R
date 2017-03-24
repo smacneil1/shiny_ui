5dc63d58dc8cc6f7ec800f257fb8a1e96807fdb0
smacneil1 <smacneil88@gmail.com>
2017-03-24 12:44:28 -0600

    cleaning up

--- server.R
+++ server.R
@@ -44,7 +44,9 @@ shinyServer(function(input, output, session) {
    if (!is.null(input$dataFile) && !is.null(input$classFile) && is.null(input$gmtFile)) {shinyjs::alert("Missing GMT File")}
    if (!is.null(input$dataFile) && !is.null(input$classFile) && !is.null(input$gmtFile))
      {shinyjs::alert("GSOA is running. An e-mail with your results will be sent shortly. ")
-     
+  
+    # add to all file names
+    #system("xxd -l 10 -p /dev/urandom")   
    system(paste("cp",input$dataFile$datapath, paste("/data", input$dataFile$name, sep="/"), sep=" "))
    system(paste("cp",input$classFile$datapath, paste("/data", input$classFile$name, sep="/"), sep=" "))
    system(paste("cp",input$gmtFile$datapath, paste("/data", input$gmtFile$name, sep="/"), sep=" "))
